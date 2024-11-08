e10f76e5d7b1645c162fc6c21a3eafb75fd3620f
Yong Wai <65732630+Yonugy@users.noreply.github.com>
2024-11-08 11:07:51 +0800

    Add files via upload

---
+++ addNewContact.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Address Book</title>
+    <style>
+        body{
+            background-image: url("wallpaper.jpg");
+            background-position: center center;
+            background-size: cover;
+        }
+
+        .container{
+            background-color: white;
+            opacity: 0.8;
+            width: 500px;
+            border: 1px solid #000;
+            padding: 20px;
+            margin: 0 auto;
+            font-size: 12pt;
+        }
+
+        input[type=text],input[type=tel],input[type=email],input[type=date],textarea,select{
+            width: 90%;
+            padding: 15px;
+            margin-top: 5px;
+            margin-bottom: 22px;
+            display: inline-block;
+            font-size: 15pt;
+        }
+
+        input[type=text]:valid,input[type=tel]:valid,input[type=email]:valid,input[type=date]:valid,textarea:valid,select:valid{
+            background-color: #7EB77F;
+        }
+
+        input[type=text]:invalid,input[type=tel]:invalid,input[type=email]:invalid,input[type=date]:invalid,textarea:invalid,select:invalid{
+            background-color: #ED7B84;
+        }
+
+        .button-container{
+            display: flex;
+            gap: 10px;
+        }
+
+        button{
+            background-color: #959595;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            display: inline-block;
+            font-size: 16px;
+            transition: background-color 0.3s ease;
+            border-radius: 5px;
+        }
+
+        button:hover{
+            background-color: #b3b3b3;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>My Address Book</h1>
+        <h2>Add New Contact</h2>
+
+        <form method="post" action="#">
+            Name<br>
+            <input id="contact_name" type="text" name="contact_name" required><br>
+
+            Phone Number<br>
+            <input id="contact_phone" type="tel" name="contact_phone" required><br>
+
+            Email Address<br>
+            <input id="contact_email" type="email" name="contact_email" required><br>
+
+            Home Address<br>
+            <textarea id="contact_address" name="contact_address" required></textarea><br>
+
+            Gender<br>
+            <input type="radio" name="contact_gender" value="male" required>Male
+            <input type="radio" name="contact_gender" value="female" required>Female<br><br>
+
+            Relationship<br>
+            <select name="contact_relationship">
+                <option value="">Please select</option>
+                <option value="Family">Family</option>
+                <option value="Friend">Friend</option>
+                <option value="Colleague">Colleague</option>
+                <option value="Other">Other</option>
+            </select><br>
+
+            Date of Birth<br>
+            <input type="date" name="contact_dob" required>
+
+            <div class="button-container">
+                <button type="submit" name="submitBtn">Submit</button>
+                <button type="reset">Reset</button>
+            </div>
+        </form>
+
+        <?php
+            if (isset($_POST['submitBtn'])) {
+                include("conn.php");
+                $sql="INSERT INTO contacts (contact_name, contact_phone, contact_email,
+                contact_address, contact_gender, contact_relationship, contact_dob)
+                VALUES
+                ('$_POST[contact_name]','$_POST[contact_phone]','$_POST[contact_email]',
+                '$_POST[contact_address]','$_POST[contact_gender]','$_POST[contact_relationship]',
+                '$_POST[contact_dob]')";
+                if (!mysqli_query($con,$sql)) {
+                die('Error: ' . mysqli_error($con));
+                }
+                else {
+                    echo '<script>alert("1 record added!");
+                    window.location.href = "viewContacts.php";
+                    </script>';
+                }
+                 mysqli_close($con);
+            }
+        ?>
+        
+    </div>
+</body>
+</html>
---
+++ viewContacts.php
@@ -0,0 +1,95 @@
+<?php
+// Initialize the input value from the URL if available
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['search'];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacts</title>
+
+    <style>
+        main{
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            padding: 10px;
+            gap: 20px;
+        }
+
+        div{
+            background-color: #efd3a9;
+            width: 300px;
+            height: 450px;
+            padding: 10px;
+            border-radius: 10px;
+        }
+
+        div button{
+            border-radius: 10%;
+        }
+
+        img{
+            height:80px;
+        }
+
+    </style>
+</head>
+<body>
+    <a href="addNewContact.php">Add New Contact</a><br><br>
+    <form method="POST" action="">
+        Search:
+        <input type="text" name="search">
+        <button type="submit" name="searchBtn">Search</button>
+    </form>
+
+    <main>
+    <?php
+        include("conn.php");
+        if (!empty($name)) {
+            $sql = "SELECT * FROM contacts WHERE contact_name = '$name'";
+        }else{
+            $sql = "SELECT * FROM contacts";
+        }
+        $result = mysqli_query($con, $sql);
+        while ($row = mysqli_fetch_array($result)) {
+            if ($row['contact_gender']=="male"){
+                $img="malepfp.png";
+            }else{
+                $img="femalepfp.png";
+            }
+            echo '<div>';
+            echo "<img src=$img>";
+            echo '<h3>' . $row['contact_name'] . '</h3>';
+            echo '<p>Phone Number:<br>' . $row['contact_phone'] . '</p>';
+            echo '<p>Email:<br>' . $row['contact_email'] . '</p>';
+            echo '<p>Home Address:<br>' . $row['contact_address'] . '</p>';
+            echo '<p>Relationship:<br>' . $row['contact_relationship'] . '</p>';
+            echo '<p>Date of Birth:<br>' . $row['contact_dob'] . '</p>';
+            echo '<form method="POST" action="">';
+            echo '<button type="submit" name="deleteBtn" value="'. $row['id'] .'">Delete</button>';
+            echo '</form>';
+            echo '</div>';
+        }
+
+        if (isset($_POST['deleteBtn'])) {
+            include("conn.php");
+            $id = $_POST['deleteBtn'];
+            $sql="DELETE FROM contacts WHERE id = $id";
+            if (!mysqli_query($con,$sql)) {
+            die('Error: ' . mysqli_error($con));
+            }
+            else {
+                echo '<script>alert("Record Deleted!");
+                window.location.href = "viewContacts.php";
+                </script>';
+            }
+             mysqli_close($con);
+        }
+    ?>
+    </main>
+</body>
+</html>
